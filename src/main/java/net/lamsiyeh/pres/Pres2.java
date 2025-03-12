5f6c2f21a32dabb190123c01e08a11855df483f3
khawla lamsiyeh <61538329+khawlalam@users.noreply.github.com>
2025-03-12 21:47:52 +0000

    commit

--- src/main/java/net/lamsiyeh/pres/Pres2.java
+++ src/main/java/net/lamsiyeh/pres/Pres2.java
@@ -1,17 +1,27 @@
 package net.lamsiyeh.pres;
 
 import net.lamsiyeh.dao.IDao;
+import net.lamsiyeh.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
 public class Pres2 {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    //FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(new File("config.txt"));
         String daoClassName = sc.nextLine();
         Class cDao = Class.forName(daoClassName);
         IDao dao= (IDao) cDao.newInstance();
-        System.out.println(dao.getData());
+
+        String metierClassName = sc.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier= (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        System.out.println("RES"+metier.calcule());
+
     }
+
 }
